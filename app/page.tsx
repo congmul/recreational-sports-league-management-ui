97ea6f38036cbde6c189983ace8b5beae64d5afa
Jehyun Jung <congmul@hotmail.com>
2024-11-24 00:00:05 -0800

    UPdate banner on Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Banner from '@/app/ui/banner/banner';
+
 export default function Home() {
-  return (
-    <div className="m-5 font-bold">Home</div>
+  return (<>
+    <div className="team-page-wrapper">
+      <Banner title={'Welcome, League Management App'} />      
+    </div>
+  </>
   );
 }
