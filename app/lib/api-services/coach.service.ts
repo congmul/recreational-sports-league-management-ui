c4e1f914682966ab5a91111df30da45a9318f143
Jehyun Jung <congmul@hotmail.com>
2024-11-24 04:11:18 -0800

    Use axiosInstance

--- app/lib/api-services/coach.service.ts
+++ app/lib/api-services/coach.service.ts
@@ -1,11 +1,10 @@
-import axios from 'axios';
-const coreServiceUrl = process.env.NEXT_PUBLIC_CORE_SERVICE_URL;
 import { Coach } from '@/app/lib/models'
+import axiosInstance from '@/app/lib/utils/axiosIntercepter';
 
 export const coachService = {
     getAllCoaches: async ():Promise<Coach[] | undefined> => {
         try{
-            const { data } = await axios.get(`${coreServiceUrl}/coach`);
+            const { data } = await axiosInstance.get(`/coach`);
             return data;            
         }catch(err){
             console.log(err);
@@ -13,10 +12,18 @@ export const coachService = {
     },
     getCoach: async (id: string):Promise<Coach | undefined> => {
         try{
-            const { data } = await axios.get(`${coreServiceUrl}/coach/${id}`);
+            const { data } = await axiosInstance.get(`/coach/${id}`);
             return data;            
         }catch(err){
             console.log(err);
         }
-    }
+    },
+    deleteCoachById: async (id: string):Promise<{status: number, message: string} | undefined> => {
+        try{
+            const { data } = await axiosInstance.delete(`/coach/${id}`,);
+            return data;            
+        }catch(err){
+            console.log(err);
+        }
+    },
 }
